b0cdbb6730056aaf39f46b4b5c5c2ac3fd018b2c
johnblack77 <yuxing.li7@outlook.com>
2019-08-12 15:36:20 +0800

    0

--- wjy/sws.py
+++ wjy/sws.py
@@ -3,20 +3,25 @@ import numpy as np
 from PIL import Image
 import os
 
-Temp_WIDTH = 0
-STEP = 0
-file_dir = ''
-out_dir = ''
+Temp_WIDTH = 28
+STEP = 4
+file_dir = '/Users/john/PycharmProjects/pro1/data1/t1'
+out_dir = '/Users/john/PycharmProjects/pro1/data1/t1'
+os.mkdir(out_dir+'/out')
 for root, dirs, files in os.walk(file_dir):
-    in_str = root + files
-    out = out_dir + files
-    out = out[:-4]
-    out += '/'
-    img = cv2.imread(in_str, cv2.IMREAD_COLOR)
-    height, width, cannel = img.shape
-    # template = np.zeros((height, width, cannel),dtype=np.int8)
-
-    for i in range(0, width - Temp_WIDTH, STEP):
-        for j in range(0, height - Temp_WIDTH, STEP):
-            template = img[i:i+Temp_WIDTH, j:j+Temp_WIDTH]
-            Image.fromarray(template).convert('RGB').save(out + str(i) + '.jpg')
+    for tmp in files:
+        c=0
+        os.mkdir(out_dir + '/out/'+tmp[:-4])
+        in_str = root +'/' + tmp
+        out = out_dir+'/out/' + tmp
+        out = out[:-4]
+        out += '/'
+        print(in_str)
+        img = cv2.imread(in_str, cv2.IMREAD_ANYCOLOR)
+        height, width, cannel = img.shape
+        # template = np.zeros((height, width, cannel),dtype=np.int8)
+        for i in range(0, width - Temp_WIDTH, STEP):
+            for j in range(0, height - Temp_WIDTH, STEP):
+                template = img[i:i+Temp_WIDTH, j:j+Temp_WIDTH]
+                Image.fromarray(template).convert('RGB').save(out + str(c) + 'png')
+                c+=1
